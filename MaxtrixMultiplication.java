1f8357de05f78103f71f3006f2c59ba1926e2da5
Joshua Chen <joshuachen1@cpp.edu>
2018-10-06 20:01:42 -0700

    Test with Varying Matrix Sizes

--- MaxtrixMultiplication.java
+++ MaxtrixMultiplication.java
@@ -5,33 +5,43 @@ public class MaxtrixMultiplication {
         float[][] matrixA, matrixB, matrixC;
         int m, n, p;
 
-        m = n = p = 5;
+        // 1 Thread
+        for (int size = 4; size <= 128; size *= 2) {
+            m = n = p = size;
 
-        matrixA = fillMatrix(m, n);
-        matrixB = fillMatrix(n, p);
-        matrixC = new float[m][p];
+            matrixA = fillMatrix(m, n);
+            matrixB = fillMatrix(n, p);
+            matrixC = new float[m][p];
 
-        long timeIn = System.nanoTime();
-        matMult(matrixA, matrixB, matrixC, m, n, p);
-        long timeOut = System.nanoTime() - timeIn;
+            // 1 Thread
+            long timeIn = System.nanoTime();
+            matMult(matrixA, matrixB, matrixC, m, n, p, 1);
+            long timeOut = System.nanoTime() - timeIn;
 
-        System.out.printf("Time with 1 Thread: %d\n", timeOut);
+            System.out.printf("Time with 1 Thread (%d x %d): %d (ns)\n", m, p, timeOut);
+        }
 
+
+        /*
         System.out.println("Matrix A:");
         printMatrix(matrixA);
         System.out.println("\nMatrix B:");
         printMatrix(matrixB);
         System.out.println("\nMatrix C:");
         printMatrix(matrixC);
+        */
     }
 
-    public static void matMult (float[][] A, float[][] B, float[][] C, int m, int n, int p) {
+    public static void matMult (float[][] A, float[][] B, float[][] C, int m, int n, int p, int numThreads) {
 
         // Classical Matrix Multiplication
-        for (int row = 0; row < m; row++)
-            for (int col = 0; col < p; col++)
-                for (int i = 0; i < n; i++)
+        for (int row = 0; row < m; row++) {
+            for (int col = 0; col < p; col++) {
+                for (int i = 0; i < n; i++) {
                     C[row][col] += A[row][i] * B[i][col];
+                }
+            }
+        }
     }
 
     public static float[][] fillMatrix(int row, int col) {
