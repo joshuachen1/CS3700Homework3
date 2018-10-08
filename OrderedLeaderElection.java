8eb99e84e06c785487759e55b163bf8fca9f02f2
Joshua Chen <joshuachen1@cpp.edu>
2018-10-08 14:03:21 -0700

    Created Threads within the same monitor

--- OrderedLeaderElection.java
+++ OrderedLeaderElection.java
@@ -6,95 +6,77 @@ public class OrderedLeaderElection {
     private static class ElectedOfficial {
         String name;
         int rank;
+
+        public ElectedOfficial(String name, int rank) {
+            this.name = name;
+            this.rank = rank;
+        }
     }
 
     public static void main(String[] args) throws InterruptedException {
 
-        final int numThreads = 10;
-        ArrayList<Thread> thread = new ArrayList<>();
+        final int numThread = 10;
+        ArrayList<Thread> threads = new ArrayList<>();
 
         ArrayList<ElectedOfficial> electedOfficials = new ArrayList<>();
-        ElectedOfficial leader = new ElectedOfficial();
+        ElectedOfficial leader = new ElectedOfficial(Thread.currentThread().getName(), Integer.MIN_VALUE);
 
-        Thread rankThread = new Thread(new RankThread(electedOfficials ,leader));
+        Thread rankThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                boolean changingLeader = true;
+                while (changingLeader) {
+                    changingLeader = false;
+                    synchronized (leader) {
+                        try {
+                            // Wait for a new elected officials to be created
+                            leader.wait(5000);
+                        } catch (InterruptedException e) {
+                            System.out.println("Changing leader");
+                            changingLeader = true;
+
+                            for (int i = 0; i < electedOfficials.size(); i++) {
+                                if (electedOfficials.get(i).rank >= leader.rank) {
+                                    updateLeader(leader, electedOfficials.get(i));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        });
+
         rankThread.start();
 
-        synchronized (leader) {
-            for (int i = 0; i < numThreads; i++) {
-                electedOfficials.add(new ElectedOfficial());
-                thread.add(new Thread(new ElectedOfficialThread(rankThread, electedOfficials.get(i), leader)));
-                thread.get(i).start();
-                Thread.sleep(1000);
-                leader.wait();
-            }
-        }
-    }
+        for (int i = 0; i < numThread; i++) {
+            electedOfficials.add(new ElectedOfficial(Thread.currentThread().getName(), Integer.MIN_VALUE));
 
-    private static class RankThread implements Runnable {
-        ArrayList<ElectedOfficial> electedOfficials;
-        ElectedOfficial leader;
-
-        public RankThread(ArrayList<ElectedOfficial> electedOfficials, ElectedOfficial leader) {
-            this.electedOfficials = electedOfficials;
-            this.leader = leader;
-            this.leader.name = null;
-            this.leader.rank = Integer.MIN_VALUE;
-        }
-
-        @Override
-        public void run() {
-            boolean changingLeader = true;
-            while (changingLeader) {
-                changingLeader = false;
-                try {
-                    // Wait for a new elected officials to be created
-                    Thread.sleep(5000);
-                } catch (InterruptedException e) {
-                    synchronized (leader) {
-                        System.out.println("Changing Leader");
-                        changingLeader = true;
-
-                        for (int i = 0; i < electedOfficials.size(); i++) {
-
-                        }
-                        leader.notifyAll();
-                    }
-                }
-            }
-        }
-    }
-
-    private static class ElectedOfficialThread implements Runnable {
-        Thread rankThread;
-        ElectedOfficial elecOff;
-        ElectedOfficial leader;
-
-        public ElectedOfficialThread(Thread rankThread, ElectedOfficial electedOfficials, ElectedOfficial leader) {
-            this.rankThread = rankThread;
-            this.elecOff = electedOfficials;
-            this.leader = leader;
-        }
-
-        @Override
-        public void run() {
-            synchronized (leader) {
-                try {
-                    elecOff.name = Thread.currentThread().getName();
-                    elecOff.rank = ThreadLocalRandom.current().nextInt(Integer.MIN_VALUE, Integer.MAX_VALUE);
+            int index = i;
+            threads.add(new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    electedOfficials.get(index).name = Thread.currentThread().getName();
+                    electedOfficials.get(index).rank = ThreadLocalRandom.current().nextInt(Integer.MIN_VALUE, Integer.MAX_VALUE);
 
                     if (leader.name == null) {
-                        System.out.printf("Name: %10s \tRank: %12d \tLeader: %10s\n", elecOff.name, elecOff.rank, Thread.currentThread().getName());
+                        System.out.printf("Name: %10s \tRank: %12d \tLeader: %10s\n", electedOfficials.get(index).name, electedOfficials.get(index).rank, Thread.currentThread().getName());
                     } else {
-                        System.out.printf("Name: %10s \tRank: %12d \tLeader: %10s\n", elecOff.name, elecOff.rank, leader.name);
+                        System.out.printf("Name: %10s \tRank: %12d \tLeader: %10s\n", electedOfficials.get(index).name, electedOfficials.get(index).rank, leader.name);
                     }
 
-                    rankThread.interrupt();
-                    leader.wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
 
-            }
+                    // Notify rankThread of new elected official
+                    rankThread.interrupt();
+                }
+            }));
+
+            threads.get(i).start();
+            Thread.sleep(1000);
         }
+    }
+
+    private static void updateLeader(ElectedOfficial currLeader, ElectedOfficial newLeader) {
+        currLeader.name = newLeader.name;
+        currLeader.rank = newLeader.rank;
     }
 }
