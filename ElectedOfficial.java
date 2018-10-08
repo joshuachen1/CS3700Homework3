5ebc0813f4668e0def67f51c3bddaba6cb75813d
Joshua Chen <joshuachen1@cpp.edu>
2018-10-08 11:34:59 -0700

    ElectedOfficial Object

---
+++ ElectedOfficial.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by student on 10/8/18.
+ */
+public class ElectedOfficial {
+}
--- OrderedLeaderElection.java
+++ OrderedLeaderElection.java
@@ -3,13 +3,14 @@ import java.util.concurrent.ThreadLocalRandom;
 public class OrderedLeaderElection {
     public static void main(String[] args) throws InterruptedException {
         final int numThreads = 10;
+        ElectedOfficial[] elecOff = new ElectedOfficial[numThreads];
         Thread[] electedOfficialThread = new Thread[numThreads];
-        Thread rankThread = new Thread(new RankThread(electedOfficialThread));
+        Thread rankThread = new Thread(new RankThread(electedOfficialThread, elecOff));
 
         rankThread.start();
 
         for (int i = 0; i < numThreads; i++) {
-            electedOfficialThread[i] = new Thread(new ElectedOfficialThread(rankThread));
+            electedOfficialThread[i] = new Thread(new ElectedOfficialThread(rankThread, elecOff, i));
             electedOfficialThread[i].start();
             Thread.sleep(1000);
         }
@@ -17,9 +18,15 @@ public class OrderedLeaderElection {
 
     private static class RankThread implements Runnable {
         Thread[] electedOfficialThread;
+        Thread currentLeader;
+        int highestRank;
+        ElectedOfficial[] elecOff;
 
-        public RankThread(Thread[] electedOfficialThread) {
+        public RankThread(Thread[] electedOfficialThread, ElectedOfficial[] elecOff) {
             this.electedOfficialThread = electedOfficialThread;
+            this.currentLeader = Thread.currentThread();
+            int highestRank = Integer.MIN_VALUE;
+            this.elecOff = elecOff;
         }
 
         @Override
@@ -28,33 +35,38 @@ public class OrderedLeaderElection {
                 // Wait for a new elected officials to be created
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
-                System.out.println("Hello");
+
             }
         }
     }
 
     private static class ElectedOfficialThread implements Runnable {
         Thread rankThread;
+        ElectedOfficial elecOff;
 
-        public ElectedOfficialThread(Thread rankThread) {
+        public ElectedOfficialThread(Thread rankThread, ElectedOfficial[] elecOff, int ID) {
             this.rankThread = rankThread;
+            elecOff[ID] = new ElectedOfficial();
+            this.elecOff = elecOff[ID];
         }
+
         @Override
         public void run() {
-            String name = Thread.currentThread().getName();
-            int rank = ThreadLocalRandom.current().nextInt(Integer.MIN_VALUE, Integer.MAX_VALUE);
-            Thread leader = Thread.currentThread();
+            elecOff.name = Thread.currentThread().getName();
+            elecOff.rank = ThreadLocalRandom.current().nextInt(Integer.MIN_VALUE, Integer.MAX_VALUE);
+            elecOff.leader = Thread.currentThread();
 
             synchronized (this) {
-                try {
-                    System.out.printf("Name: %10s \tRank: %12d \tLeader: %10s\n", name, rank, leader.getName());
+                    try {
+                        System.out.printf("Name: %10s \tRank: %12d \tLeader: %10s\n", elecOff.name, elecOff.rank, elecOff.leader.getName());
 
-                    // notify the rank thread that a new elected official has been created
-                    rankThread.interrupt();
-                    wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                        // notify the rank thread that a new elected official has been created
+                        rankThread.interrupt();
+                        wait();
+                        System.out.printf("Name: %10s \tRank: %12d \tLeader: %10s\n", elecOff.name, elecOff.rank, elecOff.leader.getName());
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
             }
         }
     }
