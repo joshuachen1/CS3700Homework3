0d85b9fa6852836126775ad1c8596c6192a6aec3
Joshua Chen <joshuachen1@cpp.edu>
2018-10-08 14:07:24 -0700

    update

--- Socks.java
+++ Socks.java
@@ -1,7 +1,33 @@
-package PACKAGE_NAME;
-
-/**
- * Created by student on 10/7/18.
- */
 public class Socks {
+    String color;
+    int numSocks;
+    int currentNumSocks;
+    int totalSocks;
+
+    public Socks (String color) {
+        this.color = color;
+        numSocks = 0;
+    }
+
+    public void setTotalSocks(int totalSocks) {
+        this.totalSocks = totalSocks;
+    }
+
+    public int getTotalSocks() {
+        return totalSocks;
+    }
+
+    public void addNumSocks() {
+        numSocks += 1;
+        currentNumSocks += 1;
+        System.out.println(color + " Sock: Produced " + currentNumSocks + " of " + totalSocks + " " + color + " Socks");
+    }
+
+    public int getNumSocks() {
+        return numSocks;
+    }
+
+    public void removeSockPair() {
+        numSocks -= 2;
+    }
 }
